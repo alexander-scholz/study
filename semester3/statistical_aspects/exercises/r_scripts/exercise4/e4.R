0b1c1967eec09409b9110f94052498ea0723c167
Alexander Scholz <goobar@web.de>
2016-11-14 09:54:47 +0100

    working on ex. 4

--- semester3/statistical_aspects/exercises/r_scripts/exercise4/e4.R
+++ semester3/statistical_aspects/exercises/r_scripts/exercise4/e4.R
@@ -1,4 +1,4 @@
-data = read.table('B4A4_haendigkeit.dat', header = T)
+data = as.data.frame(read.table('exercise4/B4A4_haendigkeit.dat', header = T))
 
 # a.)
 dim(data)
@@ -35,5 +35,41 @@ hist(data$height*100, breaks = 20)
 which(is.na(data$WrHnd))
 which(is.na(data$NWHnd))
 nrow(data)
-data = data[!is.na(data$WrHnd)]
-nrow(data)
+# remove NA data rows
+data_cleaned = data[!is.na(data$WrHnd) | !is.na(data$NWHnd),]
+hist(data_cleaned$WrHnd, breaks = 30)
+hist(data_cleaned$NWHnd, breaks = 30)
+mean(data_cleaned$WrHnd)
+median(data_cleaned$WrHnd)
+mean(data_cleaned$NWHnd)
+median(data_cleaned$NWHnd)
+box = boxplot(data_cleaned$WrHnd, data_cleaned$NWHnd, axis = F)
+axis(1, at = c(1,2), labels = c('WrHnd', 'NWHnd'))
+box$stats
+
+# c.)
+hl_male = c(data_cleaned[data_cleaned$sex==1,'WrHnd'], data_cleaned[data_cleaned$sex==1,'NWHnd'])
+hl_male = hl_male[!is.na(hl_male)]
+mean(hl_male)
+hl_female = c(data_cleaned[data_cleaned$sex==2,'WrHnd'], data_cleaned[data_cleaned$sex==2,'NWHnd'])
+hl_female = hl_female[!is.na(hl_female)]
+mean(hl_female)
+wilcox.test(hl_male, hl_female, alternative = "two.sided")
+ks.test(hl_male, hl_female, alternative = 'two.sided')
+
+# d.)
+# male
+wilcox.test(data_cleaned[data_cleaned$sex==1,'WrHnd'], data_cleaned[data_cleaned$sex==1,'NWHnd'], alternative = 'two.sided')
+ks.test(data_cleaned[data_cleaned$sex==1,'WrHnd'], data_cleaned[data_cleaned$sex==1,'NWHnd'], alternative = 'two.sided')
+# female
+wilcox.test(data_cleaned[data_cleaned$sex==2,'WrHnd'], data_cleaned[data_cleaned$sex==2,'NWHnd'], alternative = 'two.sided')
+ks.test(data_cleaned[data_cleaned$sex==2,'WrHnd'], data_cleaned[data_cleaned$sex==2,'NWHnd'], alternative = 'two.sided')
+
+# e.)
+data_e_cleaned = data_cleaned[data_cleaned$Fold!=0.5 & data_cleaned$Clap!=0.5, c('WHnd', 'Fold', 'Clap')]
+library(corrplot)
+correlation_data = cor(data_e_cleaned)
+corrplot(correlation_data, method="circle")
+
+
+  
