ec67e1bba0cee12b74836ccd0960fe3d48ad9ec9
Alexander Scholz <goobar@web.de>
2016-12-19 19:08:04 +0100

    e4

--- semester3/statistical_aspects/exercises/r_scripts/exercise9/e1c.R
+++ semester3/statistical_aspects/exercises/r_scripts/exercise9/e1c.R
@@ -3,6 +3,13 @@ row.names(data) = c('array1', 'array2', 'array3')
 colnames(data) = c('AA', 'AB', 'BB')
 
 library(MASS)
-chisq.test(data[1:2,]) # p-value = 2.422e-06
-chisq.test(data[2:3,]) # p-value < 2.2e-16
-chisq.test(data[c(1,3),]) # p-value = 3.087e-16
+# Nullhypothese: Arrays sind statistisch unabhängig
+chisq.test(data[1:2,])    # X-squared = 25.862, df = 2, p-value = 2.422e-06
+chisq.test(data[2:3,])    # X-squared = 123.74, df = 2, p-value < 2.2e-16
+chisq.test(data[c(1,3),]) # X-squared = 71.429, df = 2, p-value = 3.087e-16
+
+chisq.test(data) # X-squared = 146.36, df = 4, p-value < 2.2e-16
+
+# https://de.wikipedia.org/wiki/Chi-Quadrat-Test
+0.5*10^(-(25.862/5.9915)) # 2.412802e-05
+0.5*10^(-(25.862/4.6025)) # 1.201849e-06
---
+++ semester3/statistical_aspects/exercises/r_scripts/exercise9/e4.R
@@ -0,0 +1,27 @@
+# 1.)
+text_data = read.table('exercise9/MSc_Bioinf/B9A4_mySnps.txt', header = F)
+bim_data = data.table::fread('exercise9/MSc_Bioinf/B9A4_1KG_PCA.bim')
+
+table(is.element(text_data$V1,bim_data$V2))
+
+filt = is.element(text_data$V1,bim_data$V2)
+dummy = as.character(text_data$V1[filt])
+write.table(dummy, file="exercise9/MSc_Bioinf/filt_text_data.txt", quote = F, row.names = F, col.names = F)
+
+# 2.)
+fam.data = read.table('exercise9/MSc_Bioinf/B9A4_1KG_PCA.fam', stringsAsFactors = F, sep = ' ')
+dim(fam.data) # 1092    6
+
+table(substr(fam.data$V2, 1, 3))
+afr_data = fam.data[substr(fam.data$V2, 1, 3) == 'AFR', ]
+asn_data = fam.data[substr(fam.data$V2, 1, 3) == 'ASN', ]
+asn_data = asn_data[1:nrow(afr_data),]
+eur_data = fam.data[substr(fam.data$V2, 1, 3) == 'EUR', ]
+eur_data = eur_data[1:nrow(afr_data),]
+
+all_ethnie_data = rbind(afr_data, asn_data, eur_data)
+write.table(all_ethnie_data, 'exercise9/MSc_Bioinf/ethnie_filter_data.txt', quote = F, row.names = F, col.names = F)
+
+# 3.)
+system("plink --bfile exercise9/MSc_Bioinf/B9A4_1KG_PCA --extract exercise9/MSc_Bioinf/B9A4_mySnps.txt --keep exercise9/MSc_Bioinf/ethnie_filter_data.txt --indep-pairwphase 50 5 0.2 --out 3_pruned_data")
+system('plink --bfile exercise9/MSc_Bioinf/B9A4_1KG_PCA --extract 3_prunde_data.prune.in --keep exercise9/MSc_Bioinf/ethnie_filter_data.txt --make-bed --out 4_pruned_data')
