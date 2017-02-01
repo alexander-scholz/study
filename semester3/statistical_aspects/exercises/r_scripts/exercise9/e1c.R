c3a9186f59a8d1811a95cf187ee539766ceefadd
Alexander Scholz <alex@nodev.de>
2017-02-01 14:35:42 +0100

    stat. asp. exercise 9.1 correction

--- semester3/statistical_aspects/exercises/r_scripts/exercise9/e1c.R
+++ semester3/statistical_aspects/exercises/r_scripts/exercise9/e1c.R
@@ -2,20 +2,12 @@ data = data.frame(c(125,50,100), c(250,30,500), c(125,20,400))
 row.names(data) = c('array1', 'array2', 'array3')
 colnames(data) = c('AA', 'AB', 'BB')
 
-# todo: Tabelle mit Allelhäufigkeiten erzeugen und diese dann mit chiq.test()
-#a=matrix(data=NA,ncol=2,nrow=3)
-#for (i in 1:3) {
-#  a[i,]=c(2*myTab[i,1]+myTab[i,2])
-#}
+# Tabelle mit Allelhäufigkeiten erzeugen und diese dann mit chiq.test()
+a=matrix(data=NA,ncol=2,nrow=3)
+for (i in 1:3) {
+  a[i,] = c(2*data[i,1]+data[i,2], 2*data[i,3]+data[i,2])
+}
 
 library(MASS)
 # Nullhypothese: Arrays sind statistisch unabhängig
-chisq.test(data[1:2,])    # X-squared = 25.862, df = 2, p-value = 2.422e-06
-chisq.test(data[2:3,])    # X-squared = 123.74, df = 2, p-value < 2.2e-16
-chisq.test(data[c(1,3),]) # X-squared = 71.429, df = 2, p-value = 3.087e-16
-
-chisq.test(data) # X-squared = 146.36, df = 4, p-value < 2.2e-16
-
-# https://de.wikipedia.org/wiki/Chi-Quadrat-Test
-0.5*10^(-(25.862/5.9915)) # 2.412802e-05
-0.5*10^(-(25.862/4.6025)) # 1.201849e-06
+chisq.test(a) # X-squared = 110.0076, df = 2, p-value < 2.2e-16
